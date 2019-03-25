e68bd0e6a2f8766b107bd4c1ed05b97c70710ccb
Michal <michal.beres@vsb.cz>
2019-03-25 13:04:53 +0100

    python h5 read

--- src_py/petsc_hdf5_save.py
+++ src_py/petsc_hdf5_save.py
@@ -15,16 +15,16 @@ myVector.set(-1.0)
 
 # Save *.h5 file
 ViewHDF5 = PETSc.Viewer()     # Init. Viewer
-ViewHDF5.createHDF5('grid.h5', mode=PETSc.Viewer.Mode.WRITE,comm= PETSc.COMM_WORLD)
+ViewHDF5.createHDF5('grid2.h5', mode=PETSc.Viewer.Mode.WRITE,comm= PETSc.COMM_WORLD)
 ViewHDF5.view(obj=myVector)   # Put PETSc object into the viewer
-ViewHDF5.destroy()            # Destroy Viewer
-
-# Load *.h5 file
-ViewHDF5 = PETSc.Viewer()     # Init. Viewer
-ViewHDF5.createHDF5('grid.h5', mode=PETSc.Viewer.Mode.READ,comm= PETSc.COMM_WORLD)
-ViewHDF5.view(obj=myVector)   # Put PETSc object into the viewer
-ViewHDF5.destroy()            # Destroy Viewer
-
-
-myVector.destroy()            # Destroy PETSc Vector
-DMDA3D.destroy()              # Destroy PETSc DMDA
+#ViewHDF5.destroy()            # Destroy Viewer
+#
+## Load *.h5 file
+#ViewHDF5 = PETSc.Viewer()     # Init. Viewer
+#ViewHDF5.createHDF5('grid1.h5', mode=PETSc.Viewer.Mode.READ,comm= PETSc.COMM_WORLD)
+#ViewHDF5.view(obj=myVector)   # Put PETSc object into the viewer
+#ViewHDF5.destroy()            # Destroy Viewer
+#
+#
+#myVector.destroy()            # Destroy PETSc Vector
+#DMDA3D.destroy()              # Destroy PETSc DMDA
